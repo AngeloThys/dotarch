7ff25e7dc3c9f63ea145473afa954325a422a201
Angelo Thys <thys.angelo@gmail.com>
2024-05-13 09:09:50 +0100

    Add fish scripts that control hyprland monitor rules
    - Add dock script that disables laptop monitor and enables desktop
      monitor
    - Add scale script that rescales laptop monitor to set scale
    - Add undock script that enables laptop monitor and disables desktop
      monitor

---
+++ dot_config/hypr/scripts/dock.fish
@@ -0,0 +1,7 @@
+#!/usr/bin/env fish
+
+function dock
+    hyprctl keyword monitor HDMI-A-1, disable
+    hyprctl keyword monitor HDMI-A-1, 2560x1440@144, 0x0, 1
+    hyprctl keyword monitor eDP-1, disable
+end
